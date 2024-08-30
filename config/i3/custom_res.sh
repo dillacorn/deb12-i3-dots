e702efa4de179fed153adf86fb7c417b6738ef26
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-08-30 13:46:47 +0000

    updated custom res config... it was incorrect before

--- config/i3/custom_res.sh
+++ config/i3/custom_res.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # get modeline string for xrandr
-# this is for a CRT monitor
 gtf 1440 1080 75
 
 # adding gtf output as a newmode for xrandr to use.
@@ -11,7 +10,7 @@ xrandr --newmode "1440x1080_75.00"  165.14  1440 1536 1696 1952  1080 1081 1084 
 xrandr --addmode HDMI-A-0 "1440x1080_75.00"
 
 # apply output mode to display
-xrandr --output HDMI-A-0 --mode "1440x1080_75.00"
+xrandr --output HDMI-A-0 --mode "1440x1080_75.00" --pos 0x0 --rotate normal
 
 # additional xrandr settings here
-xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240 --right-of HDMI-A-0
+xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240 --pos 1440x0 --rotate normal
