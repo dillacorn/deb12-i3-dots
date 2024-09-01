be2512d2b1b0f7d9f3cd142b7b9989dfcad6d37d
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-01 16:06:54 -0400

    Update custom_res.sh

--- config/i3/custom_res.sh
+++ config/i3/custom_res.sh
@@ -7,10 +7,10 @@ gtf 1152 864 94
 xrandr --newmode "1152x864_94.00"  134.42  1152 1232 1360 1568  864 865 868 912  -HSync +Vsync
 
 # apply newmode to specific display
-xrandr --addmode HDMI-A-0 "1152x864_94.00"
+xrandr --addmode DisplayPort-1 "1152x864_94.00"
 
 # apply output mode to display
-xrandr --output HDMI-A-0 --mode "1152x864_94.00" --pos 0x0 --rotate normal
+xrandr --output DisplayPort-1 --mode "1152x864_94.00" --pos 0x0 --rotate normal
 
 # additional xrandr settings here
 xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240 --pos 1152x0 --rotate normal
