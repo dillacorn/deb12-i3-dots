c0fc3dcfad3030c9930dad25f99494cd53404d1f
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-02 13:39:36 -0400

    changed settings for CRT.. using i3 currently due to lower refresh rate input lag on Sway (wayland)... haven't found a fix yet but have still been enjoying my time on i3

--- config/i3/custom_res.sh
+++ config/i3/custom_res.sh
@@ -7,10 +7,12 @@ gtf 1152 864 94
 xrandr --newmode "1152x864_94.00"  134.42  1152 1232 1360 1568  864 865 868 912  -HSync +Vsync
 
 # apply newmode to specific display
-xrandr --addmode DisplayPort-1 "1152x864_94.00"
+xrandr --addmode DisplayPort-2 "1152x864_94.00"
 
 # apply output mode to display
-xrandr --output DisplayPort-1 --mode "1152x864_94.00" --pos 0x0 --rotate normal
+xrandr --output DisplayPort-2 --mode "1152x864_94.00" --pos 0x0 --rotate normal
+# applied to Compaq FS7550 (CRT monitor)
 
 # additional xrandr settings here
-xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240 --pos 1152x0 --rotate normal
+# ViewSonic XG2431 (capped to 94hz to reduce crosstalk)
+xrandr --output DisplayPort-1 --mode 1920x1080 --rate 94 --pos 1152x0 --rotate normal
