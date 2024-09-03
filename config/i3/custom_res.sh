c59f988d80343f4e9dcabb7be488f94b7ed5de0d
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-03 18:03:25 -0400

    Update custom_res.sh
    increased refresh rate just a tad..

--- config/i3/custom_res.sh
+++ config/i3/custom_res.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # get modeline string for xrandr
-gtf 1152 864 94
+gtf 1152 864 94.30
 
 # adding gtf output as a newmode for xrandr to use.
-xrandr --newmode "1152x864_94.00"  134.42  1152 1232 1360 1568  864 865 868 912  -HSync +Vsync
+xrandr --newmode "1152x864_94.30"  134.85  1152 1232 1360 1568  864 865 868 912  -HSync +Vsync
 
 # apply newmode to specific display
-xrandr --addmode DisplayPort-2 "1152x864_94.00"
+xrandr --addmode DisplayPort-2 "1152x864_94.30"
+# applied to Compaq FS7550 (CRT monitor)
 
 # apply output mode to display
-xrandr --output DisplayPort-2 --mode "1152x864_94.00" --pos 0x0 --rotate normal
-# applied to Compaq FS7550 (CRT monitor)
+xrandr --output DisplayPort-2 --mode "1152x864_94.30" --pos 0x0 --rotate normal
 
 # additional xrandr settings here
 # ViewSonic XG2431 (capped to 94hz to reduce crosstalk)
