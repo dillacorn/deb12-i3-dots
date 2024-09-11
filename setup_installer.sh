5621f36806aa2cc6cfe5b56e76d6dd90fa783fd1
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-10 21:50:52 -0400

    Update setup_installer.sh

--- setup_installer.sh
+++ setup_installer.sh
@@ -143,6 +143,7 @@ update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 xdg-mime default thunar.desktop inode/directory application/x-gnome-saved-search
 
 # set permissions
-find /home/$SUDO_USER/.config/ -type d -exec chmod 777 {} +
+chmod 755 .config/
+find /home/$SUDO_USER/.config/ -type d -exec chmod 755 {} +
 
 echo "All tasks completed!"
