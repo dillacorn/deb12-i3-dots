2b6454256d3b06c12ef616303e3b37aeb319f32d
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-13 10:48:31 -0400

    new approach...

--- config/i3/scripts/rotate_configs_new.sh
+++ config/i3/scripts/rotate_configs_new.sh
@@ -6,40 +6,41 @@ CONFIG_FILE="$HOME/.config/i3/config"
 # Backup the original config file before making changes
 cp "$CONFIG_FILE" "$CONFIG_FILE.bak"
 
+# Temporary file to store the updated config
+TEMP_CONFIG="/tmp/config.tmp"
+
 # Define the mod keys section to exclude from replacement
 MOD_KEYS_START="# mod keys"
 MOD_KEYS_END="set \$mod4 Mod4"
-
-# Temporary files to store parts of the config
-TEMP_CONFIG="/tmp/config.tmp"
-TEMP_KEYS="/tmp/keys.tmp"
-
-# Extract the mod keys section
-awk "/$MOD_KEYS_START/,/$MOD_KEYS_END/ {print}" "$CONFIG_FILE" > "$TEMP_KEYS"
-# Extract the rest of the config
-awk "!/$MOD_KEYS_START/ && !/$MOD_KEYS_END/" "$CONFIG_FILE" > "$TEMP_CONFIG"
 
 # Function to check if mod4 is currently used
 function is_mod4_in_use {
     grep -q 'set \$mod4' "$CONFIG_FILE"
 }
 
-# Check if the config uses mod4 and perform the replacement
+# Process the config file
 if is_mod4_in_use; then
     echo "Switching from mod4 to mod1..."
-    sed -i 's/mod4/mod1/g' "$TEMP_CONFIG"
-    sed -i 's/Mod4/Mod1/g' "$TEMP_KEYS"
+    awk -v RS='' -v FS='\n' '
+    BEGIN {flag=0}
+    /# mod keys/ {flag=1; print; next}
+    /set \$mod4 Mod4/ {flag=0; gsub(/mod4/, "mod1"); gsub(/Mod4/, "Mod1"); print; next}
+    flag {next}
+    {print}
+    ' "$CONFIG_FILE" > "$TEMP_CONFIG"
 else
     echo "Switching from mod1 to mod4..."
-    sed -i 's/mod1/mod4/g' "$TEMP_CONFIG"
-    sed -i 's/Mod1/Mod4/g' "$TEMP_KEYS"
+    awk -v RS='' -v FS='\n' '
+    BEGIN {flag=0}
+    /# mod keys/ {flag=1; print; next}
+    /set \$mod1 Mod1/ {flag=0; gsub(/mod1/, "mod4"); gsub(/Mod1/, "Mod4"); print; next}
+    flag {next}
+    {print}
+    ' "$CONFIG_FILE" > "$TEMP_CONFIG"
 fi
 
-# Reassemble the config file
-cat "$TEMP_KEYS" "$TEMP_CONFIG" > "$CONFIG_FILE"
-
-# Clean up temporary files
-rm "$TEMP_CONFIG" "$TEMP_KEYS"
+# Overwrite the original config with the updated one
+mv "$TEMP_CONFIG" "$CONFIG_FILE"
 
 # Restart i3 to apply the changes
 i3-msg restart
