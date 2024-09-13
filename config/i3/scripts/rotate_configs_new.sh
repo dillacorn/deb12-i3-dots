713d4b5af2f0e7bc82db99cf97d20cc816f656d9
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-13 09:56:11 -0400

    commit

--- config/i3/scripts/rotate_configs_new.sh
+++ config/i3/scripts/rotate_configs_new.sh
@@ -16,29 +16,45 @@ fi
 # Function to switch to mod4
 switch_to_mod4() {
     echo "Switching to Mod4..."
+    # Backup the current config file
+    cp "$config_file" "$config_file.bak"
+
+    # Replace mod1 with mod4
     sed -i \
     -e "s/set \$mod1 $mod1/set \$mod1 $mod4/g" \
     -e "s/set \$mod4 $mod4/set \$mod4 $mod1/g" \
     -e "s/bindsym $mod1/bindsym $mod4/g" \
     -e "s/floating_modifier $mod1/floating_modifier $mod4/g" \
+    -e "s/\(bindsym ${mod1}\+\)\([0-9]\)/\1${mod4}\2/g" \
+    -e "s/\(bindsym ${mod1}\+\)\(shift\+\([0-9]\)\)/\1${mod4}shift+\3/g" \
+    -e "s/\(bindsym ${mod1}\+\)\(ctrl\+\([0-9]\)\)/\1${mod4}ctrl+\3/g" \
     "$config_file"
+    
     echo "Switched to Mod4"
 }
 
 # Function to switch to mod1
 switch_to_mod1() {
     echo "Switching to Mod1..."
+    # Backup the current config file
+    cp "$config_file" "$config_file.bak"
+
+    # Replace mod4 with mod1
     sed -i \
     -e "s/set \$mod1 $mod4/set \$mod1 $mod4/g" \
     -e "s/set \$mod4 $mod1/set \$mod4 $mod1/g" \
     -e "s/bindsym $mod4/bindsym $mod1/g" \
     -e "s/floating_modifier $mod4/floating_modifier $mod1/g" \
+    -e "s/\(bindsym ${mod4}\+\)\([0-9]\)/\1${mod1}\2/g" \
+    -e "s/\(bindsym ${mod4}\+\)\(shift\+\([0-9]\)\)/\1${mod1}shift+\3/g" \
+    -e "s/\(bindsym ${mod4}\+\)\(ctrl\+\([0-9]\)\)/\1${mod1}ctrl+\3/g" \
     "$config_file"
+    
     echo "Switched to Mod1"
 }
 
 # Print current mod keybindings for debugging
-echo "Current i3 config content:"
+echo "Current i3 config content before changes:"
 grep 'bindsym' "$config_file"
 
 # Check the current configuration and toggle
