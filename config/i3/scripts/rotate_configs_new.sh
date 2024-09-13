56457e68042bdc18e168ae63ff6a36fdac490b9a
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-13 09:46:55 -0400

    using ai because I'm stupid apparently

--- config/i3/scripts/rotate_configs_new.sh
+++ config/i3/scripts/rotate_configs_new.sh
@@ -17,17 +17,10 @@ fi
 switch_to_mod4() {
     echo "Switching to Mod4..."
     sed -i \
-    -e "s/bindsym ${mod1}+p/bindsym ${mod4}+p/g" \
-    -e "s/bindsym ${mod1}+shift+p/bindsym ${mod4}+shift+p/g" \
-    -e "s/bindsym ${mod1}+shift+up/bindsym ${mod4}+shift+up/g" \
-    -e "s/bindsym ${mod1}+shift+Return/bindsym ${mod4}+shift+Return/g" \
-    -e "s/bindsym ${mod1}+shift+c/bindsym ${mod4}+shift+c/g" \
-    -e "s/bindsym ${mod1}+shift+f/bindsym ${mod4}+shift+f/g" \
-    -e "s/bindsym ${mod1}+f/bindsym ${mod4}+f/g" \
-    -e "s/bindsym ${mod1}+y/bindsym ${mod4}+y/g" \
-    -e "s/bindsym ${mod1}+\([0-9]\)/bindsym ${mod4}+\1/g" \
-    -e "s/bindsym ${mod1}+shift+\([0-9]\)/bindsym ${mod4}+shift+\1/g" \
-    -e "s/floating_modifier ${mod1}/floating_modifier ${mod4}/" \
+    -e "s/set \$mod1 $mod1/set \$mod1 $mod4/g" \
+    -e "s/set \$mod4 $mod4/set \$mod4 $mod1/g" \
+    -e "s/bindsym $mod1/bindsym $mod4/g" \
+    -e "s/floating_modifier $mod1/floating_modifier $mod4/g" \
     "$config_file"
     echo "Switched to Mod4"
 }
@@ -36,17 +29,10 @@ switch_to_mod4() {
 switch_to_mod1() {
     echo "Switching to Mod1..."
     sed -i \
-    -e "s/bindsym ${mod4}+p/bindsym ${mod1}+p/g" \
-    -e "s/bindsym ${mod4}+shift+p/bindsym ${mod1}+shift+p/g" \
-    -e "s/bindsym ${mod4}+shift+up/bindsym ${mod1}+shift+up/g" \
-    -e "s/bindsym ${mod4}+shift+Return/bindsym ${mod1}+shift+Return/g" \
-    -e "s/bindsym ${mod4}+shift+c/bindsym ${mod1}+shift+c/g" \
-    -e "s/bindsym ${mod4}+shift+f/bindsym ${mod1}+shift+f/g" \
-    -e "s/bindsym ${mod4}+f/bindsym ${mod1}+f/g" \
-    -e "s/bindsym ${mod4}+y/bindsym ${mod1}+y/g" \
-    -e "s/bindsym ${mod4}+\([0-9]\)/bindsym ${mod1}+\1/g" \
-    -e "s/bindsym ${mod4}+shift+\([0-9]\)/bindsym ${mod1}+shift+\1/g" \
-    -e "s/floating_modifier ${mod4}/floating_modifier ${mod1}/" \
+    -e "s/set \$mod1 $mod4/set \$mod1 $mod4/g" \
+    -e "s/set \$mod4 $mod1/set \$mod4 $mod1/g" \
+    -e "s/bindsym $mod4/bindsym $mod1/g" \
+    -e "s/floating_modifier $mod4/floating_modifier $mod1/g" \
     "$config_file"
     echo "Switched to Mod1"
 }
@@ -56,7 +42,7 @@ echo "Current i3 config content:"
 grep 'bindsym' "$config_file"
 
 # Check the current configuration and toggle
-if grep -q "bindsym ${mod1}+p" "$config_file"; then
+if grep -q "set \$mod1 $mod1" "$config_file"; then
     switch_to_mod4
 else
     switch_to_mod1
