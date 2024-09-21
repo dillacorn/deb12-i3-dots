0866c2a254ba7ed334d646c80b0d8cc8395ff761
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-21 16:26:52 -0400

    gif fix?

--- config/i3/scripts/gif.sh
+++ config/i3/scripts/gif.sh
@@ -19,11 +19,11 @@ if [ -z "$BYZANZ_PID" ]; then
         # Start recording with byzanz
         echo "Starting recording in selected area for $DURATION seconds..."
         byzanz-record --duration=$DURATION --x=$(echo $GEOMETRY | awk '{print $1}') --y=$(echo $GEOMETRY | awk '{print $2}') --width=$(echo $GEOMETRY | awk '{print $3}') --height=$(echo $GEOMETRY | awk '{print $4}') "$TEMP_OUTPUT_FILE" &
-        
+
         # Store the PID of the recording process
         echo $! > "$OUTPUT_DIR/byzanz.pid"
 
-        # Wait for the recording to finish or for the user to stop it
+        # Wait for the recording to finish
         wait $!
     else
         echo "No area selected, aborting."
@@ -47,8 +47,13 @@ else
     fi
 
     # Rename the temporary file to the user-specified or default name
-    mv "$OUTPUT_DIR/temp_gif_"*.gif "$OUTPUT_DIR/$GIF_NAME.gif"
-    echo "GIF saved as $OUTPUT_DIR/$GIF_NAME.gif"
+    TEMP_OUTPUT_FILE="$OUTPUT_DIR/temp_gif_*.gif"
+    if ls $TEMP_OUTPUT_FILE 1> /dev/null 2>&1; then
+        mv $TEMP_OUTPUT_FILE "$OUTPUT_DIR/$GIF_NAME.gif"
+        echo "GIF saved as $OUTPUT_DIR/$GIF_NAME.gif"
+    else
+        echo "No temporary GIF file found to rename."
+    fi
 
     # Clean up the PID file
     rm "$OUTPUT_DIR/byzanz.pid"
