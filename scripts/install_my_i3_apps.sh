276e7c5fa13cba68a120f75ad2b742948d4548e2
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-23 09:05:15 -0400

    made install my i3 apps a choice in script.. Should have been this way before my bad..

--- scripts/install_my_i3_apps.sh
+++ scripts/install_my_i3_apps.sh
@@ -2,63 +2,73 @@
 
 set -eu -o pipefail # fail on error and report it, debug all lines
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
 sudo -n true
-test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+test $? -eq 0 || { echo -e "${RED}You should have sudo privileges to run this script.${NC}"; exit 1; }
 
-echo installing the must-have pre-requisites
-while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
-    i3
-    fonts-font-awesome
-    i3status
-    feh
-    rofi
-    scrot
-    slop
-    dunst
-    ueberzug
-    ranger
-    lxpolkit
-    lxappearance
-    micro
-    nm-tray
-    solaar
-    blueman
-    arandr
-    pavucontrol
-    pcmanfm
-    ffmpeg
-    mpv
-    cheese
-    libexiv2-27
-    qimgv
-    flameshot
-    htop
-    btop
-    curl
-    wget
-    git
-    gdebi
-    flatpak
-    octave
-    cava
-    okular
-    tigervnc-viewer
-    timeshift
-    virt-manager
-    otpclient
-    otpclient-cli
-    wireguard
-    wireplumber
-    gamemode
+# Prompt for package installation
+echo -e "\n${CYAN}Do you want to install Dillacorn's chosen Debian applications? [y/n]${NC}"
 
+# Read a single character without requiring the Enter key
+read -n1 -s choice
+
+# Check user input
+if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    echo -e "\n${GREEN}Proceeding with installation of Dillacorn's chosen Debian applications...${NC}"
+    echo -e "${CYAN}Installing applications with apt${NC}"
+    while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+        i3
+        fonts-font-awesome
+        i3status
+        feh
+        rofi
+        scrot
+        slop
+        dunst
+        ueberzug
+        ranger
+        lxpolkit
+        lxappearance
+        micro
+        nm-tray
+        solaar
+        blueman
+        arandr
+        pavucontrol
+        pcmanfm
+        ffmpeg
+        mpv
+        cheese
+        libexiv2-27
+        qimgv
+        flameshot
+        htop
+        btop
+        curl
+        wget
+        git
+        gdebi
+        flatpak
+        octave
+        cava
+        okular
+        tigervnc-viewer
+        timeshift
+        virt-manager
+        otpclient
+        otpclient-cli
+        wireguard
+        wireplumber
+        gamemode
 EOF
-)
-
-echo installing the nice-to-have pre-requisites
-echo you have 5 seconds to proceed ...
-echo or
-echo hit Ctrl+C to quit
-echo -e "\n"
-sleep 6
-
-sudo apt-get install -y tig
+    )
+else
+    echo -e "\n${YELLOW}Skipping installation of Dillacorn's chosen Debian applications.${NC}"
+    exit 0
+fi
