f2686999b05c82a62ef6a868cfde2d6afa1e73a5
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-09-30 10:59:14 -0400

    Update fix_virt-manager.sh

--- scripts/fix_virt-manager.sh
+++ scripts/fix_virt-manager.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # requires sudo!
 
+# Ensure the script is run with sudo
+if [ -z "$SUDO_USER" ]; then
+    echo "This script must be run with sudo!"
+    exit 1
+fi
+
 virsh net-autostart default
 virsh net-start default
