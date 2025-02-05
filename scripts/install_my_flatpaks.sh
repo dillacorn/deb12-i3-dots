54bd35e06d79d5f23f0c0d91668c3711ff09d534
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-02-05 08:46:55 -0500

    added tailscale/trayscale GUI and RustDesk.

--- scripts/install_my_flatpaks.sh
+++ scripts/install_my_flatpaks.sh
@@ -41,6 +41,8 @@ flatpak_apps=(
   'com.github.tchx84.Flatseal'                            # Modify Flatpak Permissions
   'io.gitlab.librewolf-community'                         # Firefox Privacy Browser
   'io.github.ungoogled_software.ungoogled_chromium'       # Degoogled Chromium-based browser
+  'com.rustdesk.RustDesk'                                 # Teamviewer replacement
+  'dev.deedles.Trayscale'                                 # Tailscale GUI unofficial client ~ tailscale needs to be installed seperately
 )
 
 # Check if Flatpak is installed; if not, install it via apt
